ac39f6f4788bd5b86cbaac9a2f319eb45d598552
An Jorge <an.jorge@icloud.com>
2023-06-06 23:47:07 +0100

    Finished app

--- DiceAPP/diceeapp/lib/main.dart
+++ DiceAPP/diceeapp/lib/main.dart
@@ -1,30 +1,66 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
+main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int rightDiceNumber = 1;
+  int leftDiceNumber = 1;
+
+  void chanceDices() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+
+    if (rightDiceNumber == leftDiceNumber ||
+        rightDiceNumber == leftDiceNumber) {
+      debugPrint('Winner');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('DICE'),
           backgroundColor: Colors.black,
-        ),
-        body: Center(
-          child: Row(
-            children: [
-              Expanded(
-                child: Image.asset('assets/images/dice-1.png'),
-              ),
-            ],
+          title: const Text(
+            'DICE GAME',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200),
           ),
         ),
+        body: Center(
+            child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  chanceDices();
+                },
+                child: Image.asset('assets/images/dice-$rightDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  chanceDices();
+                },
+                child: Image.asset('assets/images/dice-$leftDiceNumber.png'),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
